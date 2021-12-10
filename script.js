45dd5ecd9b282cb1b2f72763c62531420984e0c8
Rajkumar Murugesan <90443069+rajm688@users.noreply.github.com>
2021-12-10 11:10:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let submit = document.getElementById("button");
-submit.addEventListener("click",function click(){
-    var chars = document.getElementsByName('radio');
+let submit = document.getElementById("button");//selecting the submit button
+submit.addEventListener("click",function click(){ // adding event listner to submit
+    var chars = document.getElementsByName('radio'); // selecting radio button
     var char;
     for(var i = 0; i < 3; i++){
         if(chars[i].checked){
             char = chars[i].value;
         }
     }
-    var url =`https://hp-api.herokuapp.com/api/characters/${char}`;
-    const fetchData = async ()=>{
+    var url =`https://hp-api.herokuapp.com/api/characters/${char}`; // getting the url
+    const fetchData = async ()=>{ // using asynnc and await for connecint data from API
         try{
            const response = await fetch(url); 
             var heros = await response.json();
+            //creating Container elements
             let main = document.createElement("div");  
             let subdiv = document.createElement('div');
             subdiv.className="card-columns"
             main.className="container"
             main.id = "main" 
+            // collecting necessary data from API
             for(let i =0 ;i<heros.length; i++){
                     let name =heros[i].name;
                     let gender = heros[i].gender;
@@ -26,6 +28,7 @@ submit.addEventListener("click",function click(){
                     let innerDiv = document.createElement("div")
                     innerDiv.className = "card"
                     innerDiv.setAttribute("style","width:100%")
+                //Appending Data to HTML
                     innerDiv.innerHTML=`<img class="card-img-top" width="100px" src="${img}" alt="img not found"></img>
                                         <div class="card-body">
                                         <h4 class="card-title text-center"><u>Name:</u></h4> <h5 class="text-center">${name}</h5>
